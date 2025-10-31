7d8d59fcc1a84c88e9a70cfb20cd327570063482
nakanishi1337 <nakanishi1337@gmail.com>
2025-10-31 14:32:36 +0000

    while do 文を使ってみた

---
+++ 2_loop/loop.rb
@@ -0,0 +1,13 @@
+# print my names 10 times
+i = 0
+while i < 10 do
+  puts "My name is Alice."
+  i += 1
+end
+
+# count up 
+j = 1
+while j <= 10 do
+  puts "This is sentence number: #{j}"
+  j += 1
+end
