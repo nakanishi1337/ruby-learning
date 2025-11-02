e4cb86e8d799b03eb374d45ee80e56427255095e
nakanishi1337 <nakanishi1337@gmail.com>
2025-11-02 09:15:08 +0000

    loopのsyntaxsuger

--- 2_loop/loop.rb
+++ 2_loop/loop.rb
@@ -5,9 +5,16 @@ while i < 10 do
   i += 1
 end
 
+# syntax sugar
+10.times { puts "My name is Alice." }
+
+
 # count up 
 j = 1
 while j <= 10 do
   puts "This is sentence number: #{j}"
   j += 1
 end
+
+# syntax sugar
+1.upto(10) { |j| puts "This is sentence number: #{j}" }
